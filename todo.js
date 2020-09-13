3f5792aad2f89b59744b169e516adcfccdbee80f
Avyakt-Agrawal <avyakt.agrawal@pathways.in>
2020-09-13 13:46:03 -0400

    -te

--- todo.js
+++ todo.js
@@ -1,4 +1,4 @@
-var list = document.querySelector('#movie-list ul  '); 
+var list = document.querySelector('#movies ul  '); 
 
  list.addEventListener('click', function(e){
    if(e.target.className === 'delete'){          // We want to get where did the click happened, so className
@@ -10,3 +10,30 @@ var list = document.querySelector('#movie-list ul  ');
 
    }
  })
+ var hide=document.getElementById('hide');
+hide.addEventListener('click',function(e){
+    if(hide.checked == true){
+        list.hidden= true;
+    }else if(hide.checked == false){
+        list.hidden= false;
+    }
+});
+
+var form = document.forms['add-movie'];
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    var vall=form.querySelector('input[type="text"]').value;
+    var li = document.createElement('li');
+    var muvi = document.createElement('span');
+    var del=document.createElement('span');
+    li.appendChild(muvi);
+    li.appendChild(del);
+    li.appendChild(li);
+    muvi.textContent = vall;
+    del.textContent = 'delete';
+
+    muvi.classList.add('name');
+    del.classList.add('delete');
+});
+
+
